489d819a82ecefa7c59a90c73d6b2ed7761efd6b
Cal McLean <calmcl1@aol.com>
2020-11-25 16:20:58 +0000

    add ffmpeg to utils

--- scripts/enable_utilities.ps1
+++ scripts/enable_utilities.ps1
@@ -5,4 +5,4 @@ Disable-BingSearch
 Enable-RemoteDesktop
 Set-StartScreenOptions -EnableBootToDesktop -EnableDesktopBackgroundOnStart
 Set-ExplorerOptions -showFileExtensions -showHiddenFilesFoldersDrives -showProtectedOSFiles
-cinst aria2 ccleaner -y
+cinst aria2 ccleaner ffmpeg -y
